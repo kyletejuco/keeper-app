11a3cf997d960dcaef26067868e6a9ba08750e36
kyletejuco <kgtejuco@gmail.com>
2020-08-26 00:35:51 -0700

    add key to notes and arrow function to notes.map()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,16 +4,17 @@ import Note from './Note';
 import Footer from './Footer';
 import notes from '../notes';
 
-function createNote(note) {
-    return (<Note title={note.title} content={note.content}/>);
-}
-
-
 function App() {
     return (
         <div> 
             <Header />
-            {notes.map(createNote)}
+            {notes.map((note) => (
+                <Note 
+                    key={note.key} 
+                    title={note.title} 
+                    content={note.content}
+                />
+            ))}
             <Footer />
         </div>
         )
